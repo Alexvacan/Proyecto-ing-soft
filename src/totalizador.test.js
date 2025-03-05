b472fa90fbf7b0ee449f4f85a46fd7dd21e5792e
Alexander <alexander.vacaflores@ucb.edu.bo>
2025-03-04 21:17:20 -0400

    Resuelto conflicto y combinado funciones y todo en verde

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,7 +1,7 @@
-import mostrar_cantidad_de_items from "./totalizador";
+import { mostrarCantidadDeItems } from "./totalizador";
 
 describe("Cantidad", () => {
-    it("deberia mostrar la cantidad de items", () => {
-      expect(mostrar_cantidad_de_items(3)).toEqual(3);
-    });
+  it("debería mostrar la cantidad de items", () => {
+    expect(mostrarCantidadDeItems(3)).toEqual(3);
   });
+});
